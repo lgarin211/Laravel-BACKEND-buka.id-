9d95c67d4992ea0390312382da362ab046a19e4b
lgarin211 <lagustinus211@gmail.com>
2021-04-13 21:58:13 +0700

    mencoba meresponsifkan halaman dan mengubah beberapa tulisan dengan icon, menambahkan beberapa elemen baru dalam index dan mengubah bagian pada bt.css

--- my.js
+++ my.js
@@ -2,6 +2,15 @@
     document.getElementById("judul_div").style.display = "none"
     document.getElementById("sort_url_div").style.display = "none"
 
+    const cas=screen.height
+    if (cas<=800) {
+        document.getElementById("casing").classList = ""
+    }
+
+    // nanti hapus
+    // console.log(document.getElementById("m0"))
+    // document.getElementById("m1").style.display="none"
+
     function sa(params) {
         const as = document.getElementById("cpss1").querySelectorAll("#this1")[0].href
         document.getElementById("modalsort22").src = as
@@ -18,6 +27,13 @@
         foas2()
         const url = document.getElementById("basic-url1").value
         document.getElementById("modalsort21").src = url
+    }
+    function moving(id) {
+        document.getElementById("casing").classList = "col-md-5 col-sm-5 doc"
+        document.getElementById("pancar").innerHTML=""
+        document.getElementById("m"+id).style.display="block"
+        document.getElementById("pancar").innerHTML=document.getElementById("m"+id).innerHTML
+        document.getElementById("m"+id).style.display="none"
     }
 
     function cps2() {
