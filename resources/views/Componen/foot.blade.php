84cfcdc07f4ff51c31a150c91448783f6ad0860f
lgarin211 <lagustinus211@gmail.com>
2021-05-23 00:42:51 +0700

    as

--- resources/views/Componen/foot.blade.php
+++ resources/views/Componen/foot.blade.php
@@ -8,15 +8,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 <!-- Slick slider -->
-<script type="text/javascript" src="{{asset('/')}}vendor/js/slick.min.js"></script>
+<!-- <script type="text/javascript" src="{{asset('/')}}vendor/js/slick.min.js"></script> -->
 <!-- Progress Bar -->
-<script src="https://unpkg.com/circlebars@1.0.3/dist/circle.js"></script>
+<!-- <script src="https://unpkg.com/circlebars@1.0.3/dist/circle.js"></script> -->
 <!-- Filterable Gallery js -->
-<script type="text/javascript" src="{{asset('/')}}vendor/js/jquery.filterizr.min.js"></script>
+<!-- <script type="text/javascript" src="{{asset('/')}}vendor/js/jquery.filterizr.min.js"></script> -->
 <!-- Gallery Lightbox -->
-<script type="text/javascript" src="{{asset('/')}}vendor/js/jquery.magnific-popup.min.js"></script>
+<!-- <script type="text/javascript" src="{{asset('/')}}vendor/js/jquery.magnific-popup.min.js"></script> -->
 <!-- Counter js -->
-<script type="text/javascript" src="{{asset('/')}}vendor/js/counter.js"></script>
+<!-- <script type="text/javascript" src="{{asset('/')}}vendor/js/counter.js"></script> -->
 <!-- Ajax contact form  -->
 <script type="text/javascript" src="{{asset('/')}}vendor/js/app.js"></script>
 
@@ -27,38 +27,38 @@
 <!-- About us Skills Circle progress  -->
 <script>
     // First circle
-    new Circlebar({
-        element: "#circle-1",
-        type: "progress",
-        maxValue: "90"
-    });
+    // new Circlebar({
+    //     element: "#circle-1",
+    //     type: "progress",
+    //     maxValue: "90"
+    // });
 
     // Second circle
-    new Circlebar({
-        element: "#circle-2",
-        type: "progress",
-        maxValue: "84"
-    });
+    // new Circlebar({
+    //     element: "#circle-2",
+    //     type: "progress",
+    //     maxValue: "84"
+    // });
 
     // Third circle
-    new Circlebar({
-        element: "#circle-3",
-        type: "progress",
-        maxValue: "60"
-    });
+    // new Circlebar({
+    //     element: "#circle-3",
+    //     type: "progress",
+    //     maxValue: "60"
+    // });
 
     // Fourth circle
-    new Circlebar({
-        element: "#circle-4",
-        type: "progress",
-        maxValue: "74"
-    });
+    // new Circlebar({
+    //     element: "#circle-4",
+    //     type: "progress",
+    //     maxValue: "74"
+    // });
 </script>
 <script src="{{asset('/')}}vendor/js/fw5.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous"></script>
-<script src="{{asset('/')}}vendor/js/aos.js"></script>
+<!-- <script src="{{asset('/')}}vendor/js/aos.js"></script> -->
 <script src="{{asset('/')}}vendor/js/my.js"></script>
 <script src="{{asset('/')}}vendor/js/ajax.js"></script>
 
 <script>
-    AOS.init();
+    // AOS.init();
 </script>
--- resources/views/Componen/head.blade.php
+++ resources/views/Componen/head.blade.php
@@ -5,10 +5,10 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"> -->
     {{-- aos --}}
-    <link rel="stylesheet" href="{{asset('/')}}vendor/css/aos.css">
+    <!-- <link rel="stylesheet" href="{{asset('/')}}vendor/css/aos.css"> -->
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
     {{-- <!-- Font Awesome --> --}}
